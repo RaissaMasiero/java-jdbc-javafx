9982be86b59a502d7c123ce235c6d945350e7bfb
Raissa Masiero <raissamasi2@gmail.com>
2023-05-11 14:37:30 -0300

    Design da MainView

--- src/main/java/com/jdbcfx/javafxcomjdbc/Main.java
+++ src/main/java/com/jdbcfx/javafxcomjdbc/Main.java
@@ -3,16 +3,22 @@ package com.jdbcfx.javafxcomjdbc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
